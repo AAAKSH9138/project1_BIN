82764f6b1bf114d034ede3d0a901e976198d5457
AAAKSH9138 <68014935+AAAKSH9138@users.noreply.github.com>
2020-07-10 17:24:13 +0530

    overflow and alert system
    This code checks for the overflow in the bin and if found then it alert the concerned  person through a text message and make a buzz so that no more garbage is thrown into it.

--- gps_gsm_module.ino
+++ gps_gsm_module.ino
@@ -3,14 +3,8 @@
 #define echo     4
 int time_;
 float distance, dist_limit = 20.00;//cms
-String pilih;
-
 int buzz = 13;
-
-
-
 void setup()
-
 {
 
 Serial.begin(9600); // Setting the baud rate of GSM Module
@@ -20,64 +14,44 @@ Serial.begin(9600); // Setting the baud rate of Serial Monitor (Arduino)
 //RecieveMessage();
 
 delay(100);
- pinMode(buzz, OUTPUT);
-  pinMode(trigger, OUTPUT);
-  pinMode(echo, INPUT);
-  digitalWrite(buzz, LOW);     //set the buzzer in off mode (initial condition)
-
+pinMode(buzz, OUTPUT);
+pinMode(trigger, OUTPUT);
+pinMode(echo, INPUT);
+digitalWrite(buzz, LOW);     //set the buzzer in off mode (initial condition)
 }
-
 void loop()
-
-{ int c=ultrasonic_sensor();
+{ 
+ int c=ultrasonic_sensor();
  if (c==1)
- {
- digitalWrite(buzz,HIGH);
-
-if (Serial.available()>0)
-SendMessage();
- }
+  {
+    digitalWrite(buzz,HIGH);
+ if (Serial.available()>0)
+    SendMessage();
+  }
 else
 {
 digitalWrite(buzz,LOW);
 }
-
 }
-
-
 void SendMessage()
-
 {
-
 Serial.println("AT+CMGF=1"); //Sets the GSM Module in Text Mode
-
-delay(1000); // Delay of 1000 milli seconds or 1 second
-
-Serial.println("AT+CMGS=\"+9072433114\"\r"); // Replace x with mobile number
-
+delay(1000); 
+Serial.println("AT+CMGS=\"+9264969353\"\r"); 
 delay(1000);
-
-Serial.println("Message Send :)");// The SMS text you want to send
-
-Serial.println(pilih);
+Serial.println("Your dustbin is about to be full.Dump it now.");
 
 delay(100);
-
 Serial.println((char)26);// ASCII code of CTRL+Z
-
 delay(1000);
-
 }
-
-
-int ultrasonic_sensor(void)
+int ultrasonic_sensor()
 {
   digitalWrite(trigger, HIGH);
-  delayMicroseconds(10);
+  delay(1000);
   digitalWrite(trigger, LOW);
-  time_ = pulseIn(echo,HIGH);
+  time_ = pulseIn(echo,HIGH);//calculate rebound time of ultrasonic waves
   distance = time_*0.01725;
-  Serial.println(distance);
   if (distance <= dist_limit)
     return 1;
   else
